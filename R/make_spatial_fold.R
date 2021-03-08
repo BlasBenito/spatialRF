5d24e7f1e5b54464441815fdfa0de3e365f135a4
blas <blasbenito@gmail.com>
2021-03-08 12:52:10 +0100

    increased fold creation speed in make spatial fold

--- R/make_spatial_fold.R
+++ R/make_spatial_fold.R
@@ -66,10 +66,10 @@ make_spatial_fold <- function(
     xy.distances <- xy.distances[xy.distances != 0]
 
     #getting the minimum
-    distance.step <- min(xy.distances)
+    # distance.step <- min(xy.distances)
 
     #getting the 1%
-    # distance.step <- max(xy.distances) / 100
+    distance.step <- max(xy.distances) / 1000
 
     rm(xy.distances)
   }
